f006e1fd66ec70c4fbdc4c3f5d2daa58a89f0706
Iftach <iftahnaf@gmail.com>
2020-05-23 19:11:51 +0300

    fixed bugs with del and edit buttons

--- findContPlace.m
+++ findContPlace.m
@@ -9,44 +9,44 @@ str_4comp_zero = sprintf('#%d - zero',cont_num);
 
 counter = 0;
 
-switch cont_str
+switch cont_str{1}
     case str_4comp_gain
-        for i = 1 : con_num
+        for i = 1 : cont_num
             if strcmp(list(i),str_4comp_gain)
                 counter = counter +1;
             end
         end
         place = counter;
     case str_4comp_lead
-        for i = 1 : con_num
+        for i = 1 : cont_num
             if strcmp(list(i),str_4comp_lead)
                 counter = counter +1;
             end
         end
         place = counter;
     case str_4comp_lag
-        for i = 1 : con_num
+        for i = 1 : cont_num
             if strcmp(list(i),str_4comp_lag)
                 counter = counter +1;
             end
         end
         place = counter;
     case str_4comp_pid
-        for i = 1 : con_num
+        for i = 1 : cont_num
             if strcmp(list(i),str_4comp_pid)
                 counter = counter +1;
             end
         end
         place = counter;
     case str_4comp_notch
-        for i = 1 : con_num
+        for i = 1 : cont_num
             if strcmp(list(i),str_4comp_notch)
                 counter = counter +1;
             end
         end
         place = counter;
     case str_4comp_zero
-        for i = 1 : con_num
+        for i = 1 : cont_num
             if strcmp(list(i),str_4comp_zero)
                 counter = counter +1;
             end
