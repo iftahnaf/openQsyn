13c086b3ac9b6b95e963b951c79761f49f9b34a5
Iftach <iftahnaf@gmail.com>
2020-05-24 15:26:19 +0300

    fixed place function bugs

--- findContPlace.m
+++ findContPlace.m
@@ -1,4 +1,4 @@
-function place = findContPlace(list, cont_num, cont_str)
+function place = findContPlace(controllers,list, cont_num, cont_str)
 
 str_4comp_gain = sprintf('#%d - gain',cont_num);
 str_4comp_lead = sprintf('#%d - lead',cont_num);
@@ -8,41 +8,48 @@ str_4comp_notch = sprintf('#%d - notch',cont_num);
 str_4comp_zero = sprintf('#%d - zero',cont_num);
 
 counter = 0;
+n_gain = length(controllers.gain);
+n_lead = length(controllers.lead);
+n_lag = length(controllers.lag);
+n_notch = length(controllers.notch);
+n_pid = length(controllers.pid);
+n_pz = length(controllers.pz);
+
 
 switch cont_str{1}
     case str_4comp_gain
         for i = 1 : cont_num
-            if strcmp(list(i),sprintf('#%d - gain',i))
+            if strcmp(list(i),sprintf('#%d - gain',i)) || strcmp(list(i),sprintf('---Deleted Gain---'))  
                 counter = counter +1;
             end
         end
     case str_4comp_lead
         for i = 1 : cont_num
-            if strcmp(list(i),sprintf('#%d - lead',i))
+            if strcmp(list(i),sprintf('#%d - lead',i)) || strcmp(list(i),sprintf('---Deleted Lead---'))
                 counter = counter +1;
             end
         end
     case str_4comp_lag
         for i = 1 : cont_num
-            if strcmp(list(i),sprintf('#%d - lag',i))
+            if strcmp(list(i),sprintf('#%d - lag',i)) || strcmp(list(i),sprintf('---Deleted Lag---'))
                 counter = counter +1;
             end
         end
     case str_4comp_pid
         for i = 1 : cont_num
-            if strcmp(list(i),sprintf('#%d - pid',i))
+            if strcmp(list(i),sprintf('#%d - pid',i)) || strcmp(list(i),sprintf('---Deleted PID---'))
                 counter = counter +1;
             end
         end
     case str_4comp_notch
         for i = 1 : cont_num
-            if strcmp(list(i),sprintf('#%d - notch',i))
+            if strcmp(list(i),sprintf('#%d - notch',i)) || strcmp(list(i),sprintf('---Deleted Notch---'))
                 counter = counter +1;
             end
         end
     case str_4comp_zero
         for i = 1 : cont_num
-            if strcmp(list(i),sprintf('#%d - zero',i))
+            if strcmp(list(i),sprintf('#%d - zero',i)) || strcmp(list(i),sprintf('---Deleted PZ---'))
                 counter = counter +1;
             end
         end
